07cf58cfece378cef776aad5e9e81f09c1f64cdc
sauliyo15 <gc.saul.7@gmail.com>
2023-02-28 21:01:16 +0100

    Completados resto de archivos en bbdd

---
+++ cmds_play.js
@@ -0,0 +1,96 @@
+const { Quiz, User, Score } = require("./model.js").models;
+
+// Creacion del juego
+exports.play = async (rl) => {
+  //Obtenemos los quizzes de la base de datos
+  let quizzes = await Quiz.findAll();
+
+  //Variables para controlar el numero aleatorio y la puntuacion
+  let aleatorio,
+    puntuacion = 0;
+
+  while (quizzes.length > 0) {
+    //Obtenemos de forma aleatoria un numero (margen 0 hasta la longitud -1)
+    aleatorio = Math.floor(Math.random() * (quizzes.length - 1));
+
+    //Obtenemos el resultado de sacar por pantalla la pregunta del quizz aleatorio
+    let respuesta = await rl.questionP(quizzes[aleatorio].question);
+
+    //Comparamos la respuesta introducida con la respuesta del quizz
+    if (
+      respuesta.toLowerCase().trim() ===
+      quizzes[aleatorio].answer.toLowerCase().trim()
+    ) {
+      //Respuesta correcta: mensaje por pantalla y sumamos puntuacion
+      rl.log(`  The answer "${respuesta}" is right!`);
+      puntuacion += 1;
+    } else {
+      //Respuesta incorrecta: mensaje por pantalla
+      rl.log(`  The answer "${respuesta}" is wrong!`);
+
+      //Llamada al metodo para mostrar la puntuacion
+      mostrarPuntuacion(rl, puntuacion);
+
+      //Llamada al metodo para guardar la puntuacion en la base de datos
+      guardarPuntuacion(rl, puntuacion);
+
+      //Finalizar
+      return;
+    }
+
+    //Borramos con splice el elemento i del array
+    quizzes.splice(aleatorio, 1);
+  }
+
+  //Una vez acabado el juego exitosamente llamamos al metodo para mostrar la puntuacion
+  mostrarPuntuacion(rl, puntuacion);
+
+  //Llamada al metodo para guardar la puntuacion en la base de datos
+  guardarPuntuacion(rl, puntuacion);
+};
+
+//Metodo para sacar por pantalla la puntuacion obtenida
+function mostrarPuntuacion(rl, puntos) {
+  rl.log(`  Score: ${puntos}`);
+}
+
+//Metodo para guardar la puntuacion en la base de datos
+async function guardarPuntuacion(rl, puntos) {
+  //Obtenemos el resultado de sacar por pantalla la pregunta del quizz aleatorio
+  let nombre = await rl.questionP("Please tip your name");
+
+  //Comprobamos si hay algun usuario con ese nombre haciendo consulta a la base de datos.
+  let users = await User.findAll({ where: { name: nombre } });
+
+  //Se comprueba si se ha obtenido algun registro
+  if (users.length === 0) {
+    //No existe el usuario en la base de datos
+
+    //Creamos el usuario nuevo con su nombre y edad 0, obtiendo una instancia de el en la variable 'usuarioNuevo'
+    let usuarioNuevo = await User.create({ name: nombre, age: 0 });
+
+    //Guardamos la puntuacion en la base de datos con el id del nuevo usuario
+    await Score.create({ wins: puntos, userId: usuarioNuevo.id });
+  } else {
+    //El usuario si existe en la base de datos
+
+    //Guardamos la puntuacion en la base de datos con el id del usuario ya existente
+    await Score.create({ wins: puntos, userId: users[0].id });
+  }
+}
+
+//Mostrar todos las puntuaciones almacenadas en la base de datos
+exports.listScore = async (rl) => {
+
+  //Guardamos la consulta de las puntuaciones en la variable 'scores'
+  let scores = await Score.findAll({
+    include: [{
+      model: User,
+      as: 'scored' //mediante la propiedad scored se accede a la relacion que nos da acceso al nombre del usuario (relacion)
+    }],
+    order: [["wins", "DESC"]], //Orden descendente
+  });
+  scores.forEach((score) =>
+    rl.log(`${score.scored.name}|${score.wins}|${score.createdAt.toUTCString()}`)
+  );
+};
---
+++ cmds_quiz.js
@@ -0,0 +1,84 @@
+const {User, Quiz} = require("./model.js").models;
+
+// Show all quizzes in DB including <id> and <author>
+exports.list = async (rl) =>  {
+
+  let quizzes = await Quiz.findAll(
+    { include: [{
+        model: User,
+        as: 'author'
+      }]
+    }
+  );
+  quizzes.forEach( 
+    q => rl.log(`  "${q.question}" (by ${q.author.name}, id=${q.id})`)
+  );
+}
+
+// Create quiz with <question> and <answer> in the DB
+exports.create = async (rl) => {
+
+  let name = await rl.questionP("Enter user");
+    let user = await User.findOne({where: {name}});
+    if (!user) throw new Error(`User ('${name}') doesn't exist!`);
+
+    let question = await rl.questionP("Enter question");
+    if (!question) throw new Error("Response can't be empty!");
+
+    let answer = await rl.questionP("Enter answer");
+    if (!answer) throw new Error("Response can't be empty!");
+
+    await Quiz.create( 
+      { question,
+        answer, 
+        authorId: user.id
+      }
+    );
+    rl.log(`   User ${name} creates quiz: ${question} -> ${answer}`);
+}
+
+// Test (play) quiz identified by <id>
+exports.test = async (rl) => {
+
+  let id = await rl.questionP("Enter quiz Id");
+  let quiz = await Quiz.findByPk(Number(id));
+  if (!quiz) throw new Error(`  Quiz '${id}' is not in DB`);
+
+  let answered = await rl.questionP(quiz.question);
+
+  if (answered.toLowerCase().trim()===quiz.answer.toLowerCase().trim()) {
+    rl.log(`  The answer "${answered}" is right!`);
+  } else {
+    rl.log(`  The answer "${answered}" is wrong!`);
+  }
+}
+
+// Update quiz (identified by <id>) in the DB
+exports.update = async (rl) => {
+
+  let id = await rl.questionP("Enter quizId");
+  let quiz = await Quiz.findByPk(Number(id));
+
+  let question = await rl.questionP(`Enter question (${quiz.question})`);
+  if (!question) throw new Error("Response can't be empty!");
+
+  let answer = await rl.questionP(`Enter answer (${quiz.answer})`);
+  if (!answer) throw new Error("Response can't be empty!");
+
+  quiz.question = question;
+  quiz.answer = answer;
+  await quiz.save({fields: ["question", "answer"]});
+
+  rl.log(`  Quiz ${id} updated to: ${question} -> ${answer}`);
+}
+
+// Delete quiz & favourites (with relation: onDelete: 'cascade')
+exports.delete = async (rl) => {
+
+  let id = await rl.questionP("Enter quiz Id");
+  let n = await Quiz.destroy({where: {id}});
+  
+  if (n===0) throw new Error(`  ${id} not in DB`);
+  rl.log(`  ${id} deleted from DB`);
+}
+
