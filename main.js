c2dfa843b25fd2e53726a6204791f56347ed2fa3
sauliyo15 <gc.saul.7@gmail.com>
2023-03-13 17:53:46 +0100

    Integracion Servidor Web Express

--- main.js
+++ main.js
@@ -1,82 +1,329 @@
-const user = require("./cmds_user.js");
-const quiz = require("./cmds_quiz.js");
-const favs = require("./cmds_favs.js");
+const express = require('express');
+const app = express();
 
-//Importamos el modulo para el juego
-const play = require("./cmds_play.js");
+// Import MW for parsing POST params in BODY
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const readline = require("readline");
+// Import MW supporting Method Override with express
+const methodOverride = require('method-override');
+app.use(methodOverride('_method', { methods: ["POST", "GET"] }));
 
-//Importamos el modulo net
-let net = require("net");
+// ========== MODEL ==========
 
-//Puero en el que opera el servidor
-let puerto = 8080;
+const Sequelize = require('sequelize');
 
-//Creacion del socket de servidor -- con el manejador de conexiones y eventos
-let servidor = net.createServer((socket) => {
-  
-  //Creamos el interfaz de entrada y salida de datos con el mismo socket
-  const rl = readline.createInterface({
-    input: socket,
-    output: socket,
-    prompt: "> ",
-  });
-  rl.log = (msg) => socket.write(msg); // Add log to rl interface
-  rl.questionP = function (string) {
-    // Add questionP to rl interface
-    return new Promise((resolve) => {
-      this.question(`  ${string}: `, (answer) => resolve(answer.trim()));
-    });
-  };
+const options = { logging: false, operatorsAliases: 0 };
+const sequelize = new Sequelize("sqlite:db.sqlite", options);
 
-  rl.prompt();
-
-  //Manejador del evento 'line'
-  rl.on("line", async (line) => {
-    try {
-      let cmd = line.trim();
-
-      if ("" === cmd) {} 
-      else if ("h" === cmd) {user.help(rl);} 
-      else if (["lu", "ul", "u"].includes(cmd)) {await user.list(rl);} 
-      else if (["cu", "uc"].includes(cmd)) {await user.create(rl);} 
-      else if (["ru", "ur", "r"].includes(cmd)) {await user.read(rl);} 
-      else if (["uu"].includes(cmd)) {await user.update(rl);} 
-      else if (["du", "ud"].includes(cmd)) {await user.delete(rl);}
-
-      else if (["lq", "ql", "q"].includes(cmd)) {await quiz.list(rl);}
-      else if (["cq", "qc"].includes(cmd)) {await quiz.create(rl);} 
-      else if (["tq", "qt", "t"].includes(cmd)) {await quiz.test(rl);} 
-      else if (["uq", "qu"].includes(cmd)) {await quiz.update(rl);} 
-      else if (["dq", "qd"].includes(cmd)) {await quiz.delete(rl);}
-
-      else if (["lf", "fl", "f"].includes(cmd)) {await favs.list(rl);} 
-      else if (["cf", "fc"].includes(cmd)) {await favs.create(rl);} 
-      else if (["df", "fd"].includes(cmd)) {await favs.delete(rl);}
-
-      else if ('p' ===cmd)  { play.play(rl);}
-      else if (['ls'].includes(cmd))      { await play.listScore(rl);}
-
-      else if ("e" === cmd) {
-        rl.log("Bye!");
-        socket.destroy();
-        console.log("Ckiente desconectado");
-      } else {
-        rl.log("UNSUPPORTED COMMAND!");
-        user.help(rl);
-      }
-    } catch (err) {
-      rl.log(`  ${err}`);
-    } finally {
-      rl.prompt();
+const Quiz = sequelize.define( // define Quiz model (table quizzes)
+    'quiz', {
+        question: Sequelize.STRING,
+        answer: Sequelize.STRING
     }
-  });
+);
+
+//---------------NO NECESARIA LA INICIALIZACION DE LA BB.DD-------------------
+/*(async () => {  // IIFE - Immediatedly Invoked Function Expresión
+    try {
+        await sequelize.sync(); // Syncronize DB and seed if needed
+        const count = await Quiz.count();
+        if (count === 0) {
+            const c = await Quiz.bulkCreate([
+                {question: "Capital of Italy", answer: "Rome"},
+                {question: "Capital of France", answer: "Paris"},
+                {question: "Capital of Spain", answer: "Madrid"},
+                {question: "Capital of Portugal", answer: "Lisbon"}
+            ]);
+            console.log(`DB filled with ${c.length} quizzes.`);
+        } else {
+            console.log(`DB exists & has ${count} quizzes.`);
+        }
+    } catch (err) {
+        console.log(err);
+    }
+})();*/
+
+// ========== VIEWs ==========
+// CSS style to include into the views:
+const style = `
+        <style>
+            .button { display: inline-block; text-decoration: none;
+                padding: 2px 6px; margin: 2px;
+                background: #4479BA; color: #FFF;
+                border-radius: 4px; border: solid 1px #20538D; }
+            .button:hover { background: #356094; }
+        </style>`;
+
+// View to display all the quizzes in quizzes array
+const indexView = quizzes =>
+    `<!doctype html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <title>Quiz</title>
+        ${style}
+    </head>
+    <body>
+        <h1>Quizzes</h1>` +
+    quizzes.map(quiz =>
+        `<div>
+                <a href="/quizzes/${quiz.id}/play">${quiz.question}</a>
+                <a href="/quizzes/${quiz.id}/edit" class="button">Edit</a>
+                <a href="/quizzes/${quiz.id}?_method=DELETE" onClick="return confirm('Delete: ${quiz.question}')" class="button">Delete</a>
+             </div>`).join("\n") +
+    `<a href="/quizzes/new" class="button">New Quiz</a>
+    </body>
+    </html>`;
+
+
+// View with form for trying to guess quiz
+// response - text of last trial (hidden param)
+const playView = (quiz, response) =>
+    `<!doctype html>
+  <html>
+  <head>
+      <meta charset="utf-8">
+      <title>Quiz</title>
+      ${style}
+  </head>
+  <body>
+      <h1>Play Quiz</h1>
+      <form method="get" action="/quizzes/${quiz.id}/check">
+          <label for="response">${quiz.question}: </label>
+          <br>
+          <input type="text" name="response" value="${response}" placeholder="Answer">
+          <input type="submit" class="button" value="Check">
+      </form>
+      <br>
+      <a href="/quizzes" class="button">Go back</a>
+  </body>
+  </html>`;
+
+
+// View with the result of trying to guess the quiz.
+// id - played quiz id
+// msg - result of trial
+// response - user answer for next trial
+const resultView = (id, msg, response) =>
+    `<!doctype html>
+  <html>
+  <head>
+    <meta charset="utf-8">
+    <title>Quiz</title>
+      ${style}
+  </head>
+  <body>
+    <h1>Result</h1>
+    <div id="msg"><strong>${msg}</strong></div>
+    <a href="/quizzes" class="button">Go back</a>
+    <a href="/quizzes/${id}/play?response=${response}" class="button">Try again</a>
+  </body>
+  </html>`;
+
+
+// View to show the form to create a new quiz.
+const newView = quiz => {
+    return `<!doctype html>
+  <html>
+  <head>
+    <meta charset="utf-8">
+    <title>Quiz</title>
+    ${style}
+  </head>
+  <body>
+    <h1>Create New Quiz</h1>
+    <form method="POST" action="/quizzes">
+      <label for="question">Question: </label>
+      <input type="text" name="question" value="${quiz.question}" placeholder="Question"> 
+      <br>
+      <label for="answer">Answer: </label>
+      <input type="text" name="answer" value="${quiz.answer}" placeholder="Answer">
+      <input type="submit" class="button" value="Create">
+    </form>
+    <br>
+    <a href="/quizzes" class="button">Go back</a>
+  </body>
+  </html>`;
+}
+
+
+// View to show a form to edit a given quiz.
+const editView = (quiz) => {
+    return `<!doctype html>
+    <html>
+    <head>
+      <meta charset="utf-8">
+      <title>Quiz</title>
+      ${style}
+    </head>
+    <body>
+      <h1>Edit A Quiz</h1>
+      <form method="POST" action="/quizzes/${quiz.id}?_method=PUT">
+        <label for="question">Question: </label>
+        <input type="text" name="question" value="${quiz.question}" placeholder="Question"> 
+        <br>
+        <label for="answer">Answer: </label>
+        <input type="text" name="answer" value="${quiz.answer}" placeholder="Answer">
+        <input type="submit" class="button" value="Update">
+      </form>
+      <br>
+      <a href="/quizzes" class="button">Go back</a>
+    </body>
+    </html>`;
+}
+
+
+// ========== CONTROLLERs ==========
+
+// GET /, GET /quizzes
+const indexController = async (req, res, next) => {
+    try {
+        const quizzes = await Quiz.findAll()
+        res.send(indexView(quizzes))
+    } catch (err) {
+        next(err);
+    }
+};
+
+//  GET  /quizzes/:id/play
+const playController = async (req, res, next) => {
+    const id = Number(req.params.id);
+    if (Number.isNaN(id)) return next(new Error(`"${req.params.id}" should be number.`));
+
+    const response = req.query.response || "";
+
+    try {
+        const quiz = await Quiz.findByPk(id);
+        if (quiz) res.send(playView(quiz, response));
+        else next(new Error(`Quiz ${id} not found.`));
+    } catch (err) {
+        next(err)
+    }
+};
+
+//  GET  /quizzes/:id/check
+const checkController = async (req, res, next) => {
+    const response = req.query.response;
+
+    const id = Number(req.params.id);
+    if (Number.isNaN(id)) return next(new Error(`"${req.params.id}" should be number.`));
+
+    try {
+        const quiz = await Quiz.findByPk(id);
+        if (!quiz) return next(new Error(`Quiz ${id} not found.`));
+        let msg = (quiz.answer.toLowerCase().trim() === response.toLowerCase().trim())
+            ? `Yes, "${response}" is the ${quiz.question}`
+            : `No, "${response}" is not the ${quiz.question}`;
+        res.send(resultView(id, msg, response));
+    } catch (err) {
+        next(err)
+    }
+};
+
+// GET /quizzes/new
+const newController = async (req, res, next) => {
+    const quiz = {question: "", answer: ""};
+    res.send(newView(quiz));
+};
+
+// POST /quizzes
+const createController = async (req, res, next) => {
+    const {question, answer} = req.body;
+
+    try {
+        await Quiz.create({question, answer});
+        res.redirect(`/quizzes`);
+    } catch (err) {
+        next(err)
+    }
+};
+
+//  GET /quizzes/:id/edit
+const editController = async (req, res, next) => {
+    const id = Number(req.params.id);
+    if (Number.isNaN(id)) return next(new Error(`"${req.params.id}" should be number.`));
+
+    try {
+        const quiz = await Quiz.findByPk(id);
+        if (quiz) res.send(editView(quiz));
+        else next(new Error(`Quiz ${id} not found.`));
+    } catch (err) {
+        next(err)
+    }
+};
+
+//  PUT /quizzes/:id
+const updateController = async (req, res, next) => {
+    let pregunta = req.body.question;
+    let respuesta = req.body.answer;
+    let id = req.params.id;
+
+    try {
+        const quiz = await Quiz.findByPk(id);
+        if (quiz) {
+            await Quiz.update(
+                {question: pregunta, answer: respuesta},
+                {where: {id: id}}
+            );
+            res.redirect(`/quizzes`);
+        }
+        else next(new Error(`Quiz ${id} not found.`));
+    } catch (err) {
+        next(err)
+    }
+
+};
+
+// DELETE /quizzes/:id
+const destroyController = async (req, res, next) => {
+    
+    let id = req.params.id;
+
+    try {
+        const quiz = await Quiz.findByPk(id);
+        if (quiz) {
+            await Quiz.destroy(
+                {where: {id: id}}
+            );
+            res.redirect(`/quizzes`);
+        }
+        else next(new Error(`Quiz ${id} not found.`));
+    } catch (err) {
+        next(err)
+    }
+};
+
+
+// ========== ROUTES ==========
+
+app.get(['/', '/quizzes'], indexController);
+app.get('/quizzes/:id/play', playController);
+app.get('/quizzes/:id/check', checkController);
+app.get('/quizzes/new', newController);
+app.post('/quizzes', createController);
+
+app.get('/quizzes/:id/edit', editController);
+app.put('/quizzes/:id', updateController);
+app.delete('/quizzes/:id', destroyController);
+
+
+// ..... crear rutas e instalar los MWs para:
+//   GET  /quizzes/:id/edit
+//   PUT  /quizzes/:id
+//   DELETE  /quizzes/:id
+
+
+app.all('*', (req, res) =>
+    res.status(404).send("Error: resource not found or method not supported.")
+);
+
+
+// Middleware to manage errors:
+app.use((error, req, res, next) => {
+    console.log("Error:", error.message || error);
+    res.redirect("/");
 });
 
-//Se establece el puerto en el que va a escuchar las conexiones
-servidor.listen(puerto);
-
-//Mensaje por pantalla
-console.log("Servidor arrancado y escuchando en el puerto: " + puerto);
-
+// Server started at port 8000
+app.listen(8000, () => console.log("Servidor Express Web funcionando en el puerto 8000"));
